29ad4faceb51cde66cc94b3b7994053cc4bdb619
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-03-21 17:34:39 +0300

    FinancialTransactions actions updated [skip ci]

--- src/SDK/Action/FinancialTransactions.php
+++ src/SDK/Action/FinancialTransactions.php
@@ -12,7 +12,7 @@ class FinancialTransactions extends ActionAbstract implements ActionInterface
     {
         $this->action = 'SALE';
         $queryParamKeys = [
-            'CUSTOMER', 'AMOUNT', 'CURRENCY', 'CUSTOMEREMAIL', 'CUSTOMERNAME', 'CUSTOMERIP',
+            'MERCHANTPAYMENTID', 'CUSTOMER', 'AMOUNT', 'CURRENCY', 'CUSTOMEREMAIL', 'CUSTOMERNAME', 'CUSTOMERIP',
             'CARDHOLDERPORT', 'CUSTOMERUSERAGENT', 'CARDTOKEN', 'CARDPAN', 'CARDEXPIRY',
             'NAMEONCARD', 'CARDCVV', 'CUSTOMERPHONE', 'SAVECARD', 'ISREFUNDABLE', 'CARDSAVENAME',
             'INSTALLMENTS', 'PAYMENTSYSTEM', 'POINTS', 'CAMPAIGNS', 'BILLTOADDRESSLINE', 'BILLTOCITY',
@@ -21,26 +21,52 @@ class FinancialTransactions extends ActionAbstract implements ActionInterface
             'DEALERCODE', 'FORGROUP'
         ];
         $args = MerchantSafeUnipay\filter($queryParamKeys, $args);
-        $this->queryParameters = $args;
+        $this->queryParameters = array_merge($this->merchantParams, $args);
     }
 
     public function preAuth($args)
     {
         $this->action = 'PREAUTH';
+        $queryParamKeys = [
+            'MERCHANTPAYMENTID', 'CUSTOMER', 'CARDHOLDERPORT', 'CARDTOKEN', 'AMOUNT', 'CURRENCY', 'CARDPAN',
+            'CARDEXPIRY', 'NAMEONCARD', 'CUSTOMEREMAIL', 'CUSTOMERNAME', 'CUSTOMERPHONE', 'CUSTOMERIP',
+            'CUSTOMERUSERAGENT', 'INSTALLMENTS', 'PAYMENTSYSTEM','CARDCVV', 'SAVECARD', 'ISREFUNDABLE',
+            'CARDSAVENAME', 'BILLTOADDRESSLINE', 'BILLTOCITY', 'BILLTOPOSTALCODE', 'BILLTOCOUNTRY',
+            'SHIPTOADDRESSLINE', 'SHIPTOCITY', 'SHIPTOPOSTALCODE', 'SHIPTOCOUNTRY', 'THREATMETRIXSESSIONID',
+            'TMXSESSIONQUERYINPUT', 'EXTRA', 'DEALERCODE', 'FORGROUP'
+
+        ];
+        $args = MerchantSafeUnipay\filter($queryParamKeys, $args);
+        $this->queryParameters = $args;
     }
 
     public function postAuth($args)
     {
         $this->action = 'POSTAUTH';
+        $queryParamKeys = [
+            'MERCHANTPAYMENTID', 'PGTRANID', 'AMOUNT'
+        ];
+        $args = MerchantSafeUnipay\filter($queryParamKeys, $args);
+        $this->queryParameters = $args;
     }
 
     public function void($args)
     {
         $this->action = 'VOID';
+        $queryParamKeys = [
+            'MERCHANTPAYMENTID', 'PGTRANID',
+        ];
+        $args = MerchantSafeUnipay\filter($queryParamKeys, $args);
+        $this->queryParameters = $args;
     }
 
     public function refund($args)
     {
         $this->action = 'REFUND';
+        $queryParamKeys = [
+            'AMOUNT', 'PGTRANID', 'CURRENCY', 'MERCHANTPAYMENTID', 'SUBSTATUS'
+        ];
+        $args = MerchantSafeUnipay\filter($queryParamKeys, $args);
+        $this->queryParameters = $args;
     }
 }
