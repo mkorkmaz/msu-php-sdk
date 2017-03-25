3c0ff7c1244ad7241ec8f86d574ad2d79d078cc9
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2017-03-26 01:41:35 +0300

    Renamed class [skip ci]

--- src/SDK/Action/DealerPst.php
+++ src/SDK/Action/DealerPst.php
@@ -5,7 +5,7 @@ namespace MerchantSafeUnipay\SDK\Action;
 
 use MerchantSafeUnipay;
 
-class DealerPST extends ActionAbstract implements ActionInterface
+class DealerPst extends ActionAbstract implements ActionInterface
 {
     public function add($args)
     {
